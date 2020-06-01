07658020d23c4b35f91afab31c97984ce17d249e
Max Starkman <max.starkman@gmail.com>
2020-05-31 22:06:08 -0400

    completed task 3

---
+++ orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../Satellite';
+
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+
+//
+
+export class OrbitListComponent implements OnInit {
+
+  @Input() satellites: Satellite[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
